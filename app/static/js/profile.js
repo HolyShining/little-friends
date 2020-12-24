51a96f4a6a3195669038959e3fb1230901b3a661
karitrif <71696405+karitrif@users.noreply.github.com>
2020-12-24 03:31:57 +0200

    Update profile.js

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -1,11 +1,3 @@
-// function show_div() {var d = document;
-// d.querySelector('.hide_div').addEventListener('click', function(){
-//     var div3 = d.createElement('div');
-//     div3.className = 'new_pet';
-//     div3.innerHTML = '<a href=""><img class="avatar"src="../templates/img/1.jpg"></img></a><p>newpet</p>';
-//     d.querySelector('.petlist').appendChild(div3);
-// }, false);}
-
 function show_addpet() {
   if (document.getElementById("p1_hidden").style.visibility == "visible"){
     document.getElementById("p1_hidden").style.visibility = "hidden";
@@ -32,15 +24,3 @@ function show_del() {
     document.getElementById("p3_hidden").style.visibility = "visible";
   };
 }
-
-
-// function add_newpet(){
-//   var d = document;
-//   d.querySelector('.add_pet').addEventListener('mouseover', function(){
-//     var p = d.getElementsByClassName('p_hidden');
-//     var actualDisplay = window.getComputedStyle(p).display;
-//     if (actualDisplay == 'none'){
-//       added_item_button.style.display = 'inline';
-//     }
-//   },false);
-// }s
